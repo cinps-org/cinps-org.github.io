7b36d9596bb917898f2eee2c0878b04eeb643fcc
cinps-org <cinpsnotifications@gmail.com>
2026-02-15 14:15:29 +0530

    Create firebase-firestore.js

---
+++ firebase/firebase-firestore.js
@@ -0,0 +1,10 @@
+// firebase/firebase-firestore.js
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { app } from "./firebase-config.js";
+
+const db = getFirestore(app);
+
+export { db };
